aca0e119b4eff6e864c2259546e7112e58d68cb8
Lin1197757941 <1197757941@qq.com>
2022-05-21 17:53:45 +0800

    Signed-off-by: Lin1197757941 <1197757941@qq.com>

--- playthread.h
+++ playthread.h
@@ -1,6 +1,7 @@
 #ifndef PLAYTHREAD_H
 #define PLAYTHREAD_H
 #include "multimedia_decode_module/ffmpegmultimedia.h"
+#include<QMutex>
 class PlayThread:public QObject
 {
     Q_OBJECT
@@ -11,7 +12,6 @@ public:
     void openFile(QString url);//打开视频文件
     void initdevice();//初始化设备
     void initTimer();//初始化计时器
-    void close();//停止
     void fastplay(qreal i);
 public slots:
     void dowork();//启动
@@ -19,8 +19,10 @@ public slots:
     void pause();//播放或暂停
     void fastforward();
     void fastback();
+    void close();//停止
 signals:
     void updateImage(QImage img);//更新Image
+
 private:
     lzq::FFMpegMultimedia *ffmpeg;
     QTimer *ptimer;
@@ -28,7 +30,8 @@ private:
     QAudioFormat format;
     bool playFlag;//播放启动
     bool playState;//播放状态
-    qreal fast=1;
+    qreal fast;
+    QMutex mutex;
 public:
     QIODevice *device;
 };
