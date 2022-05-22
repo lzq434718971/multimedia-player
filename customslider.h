940a50c5ce786b9a62a4a8a88e2289148bdb1779
Treelhj <1553504297@qq.com>
2022-05-22 21:31:29 +0800

    自定义slider类

---
+++ customslider.h
@@ -0,0 +1,25 @@
+#ifndef CUSTOMSLIDER_H
+#define CUSTOMSLIDER_H
+
+#include <QSlider>
+#include <QMouseEvent>
+#include <QCoreApplication>
+
+class CustomSlider : public QSlider
+{
+    Q_OBJECT
+public:
+    CustomSlider(QWidget *parent = 0);
+
+
+protected:
+    void mousePressEvent(QMouseEvent *ev);//重写QSlider的mousePressEvent事件
+signals:
+    void costomSliderClicked();//自定义的鼠标单击信号，用于捕获并处理
+
+
+public:
+    bool ismove;
+};
+
+#endif // CUSTOMSLIDER_H
